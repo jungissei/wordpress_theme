37f7d04e2234e67fa1fd8b26704c49c5671993d0
Jung Issei <isseeeei@gmail.com>
2024-08-11 23:02:27 +0900

    バージョン変数追加

--- html/wp-content/themes/boilerplate/includes/form/form.php
+++ html/wp-content/themes/boilerplate/includes/form/form.php
@@ -8,8 +8,12 @@ add_filter('wpcf7_autop_or_not', function(){
 
 // フォームのCSS,JSを読み込み
 function form_enqueue_scripts() {
+
   add_action( 'wp_enqueue_scripts', function () {
-    wp_enqueue_style( 'form-common', get_theme_file_uri( '/includes/form/lib/css/style.css' ), [], 0);
-    wp_enqueue_script( 'form-common', get_theme_file_uri( '/includes/form/lib/js/scripts.js' ), ['jquery'], 1);
+
+    $form_assets_version = 1;
+
+    wp_enqueue_style( 'form-common', get_theme_file_uri( '/includes/form/lib/css/style.css' ), [], $form_assets_version);
+    wp_enqueue_script( 'form-common', get_theme_file_uri( '/includes/form/lib/js/scripts.js' ), ['jquery'], $form_assets_version);
   });
 }
